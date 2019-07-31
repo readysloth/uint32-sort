b6fc21c47c3bfe9c20576d2550b8f7ee04b38a27
readysloth <readysloth@protonmail.com>
2019-07-31 09:12:46 +0300

    Update watch_sort.sh
    Удалил передачу строк в column, так как это сильно нагружало процессор

--- watch_sort.sh
+++ watch_sort.sh
@@ -3,6 +3,6 @@
 UINTS_CNT=$(($1/4))
 watch -n0 -d "
 echo 'Отступ от начала файла равен' $(($1/(1024**2))) 'MB';
-od -vtu4 -An -w4 -j $1 copied.garbage | column
+od -vtu4 -An -w4 -j $1 copied.garbage
 "
 
