05d0d6a683cd0e941dc0c25d22eba3e54d87b3e3
readysloth <readysloth@protonmail.com>
2019-08-06 20:49:23 +0300

    Сделал скрипт watch_sort гибче

--- watch_sort.sh
+++ watch_sort.sh
@@ -3,6 +3,7 @@
 UINTS_CNT=$(($1/4))
 watch -n0 -d "
 echo 'Отступ от начала файла равен' $(($1/(1024**2))) 'MB';
-od -vtu4 -An -w4 -j $1 copied.garbage
+echo 'Файл ' $(ls -lh | cut -d' ' -f5 | sed -n '2p') '$2'
+od -vtu4 -An -w4 -j $1 $2 | head -n 40
 "
 
